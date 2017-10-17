33ca470c5f09c75a656f945eed980e61a54bd375
Maya Leonardo <mleonardo@gmail.com>
2017-10-17 03:45:00 -0400

    added more album data

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
   $("album-title").text(album.title);
   $("img#album-cover-art").attr("src", album.albumArtUrl);
+  $(".artist").text(album.artist);
+  $("div#release-info").html("<p>" + album.releaseInfo + "</p>");
 });
